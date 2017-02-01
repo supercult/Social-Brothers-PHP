d78ed008cfe2935a6a167914260a63b5b4bce48e
supenogaming <supenogaming@gmail.com>
2017-02-01 10:48:47 -0500

    removed details

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ require_once 'include/facebook.php';
 /*
  * Configuration and setup FB API
  */
-$appId = '1636343336672953'; //Facebook App ID
-$appSecret = 'f4135ace4df1c5977ed39e9bf5cd1cdb'; // Facebook App Secret
+$appId = ''; //Facebook App ID
+$appSecret = ''; // Facebook App Secret
 $redirectURL = 'http://social-brothers-php-supenogaming450820.codeanyapp.com/'; // Callback URL
 $fbPermissions = 'email';  //Required facebook permissions
 
